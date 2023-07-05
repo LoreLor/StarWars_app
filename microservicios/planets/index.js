8a8f89902be099117f3ed32d469a94f767089805
Lorena De Armas <lorenadearmas01@gmail.com>
2023-07-05 14:55:01 -0300

    ignore

--- microservicios/planets/index.js
+++ microservicios/planets/index.js
@@ -2,5 +2,5 @@ const server = require('./src/server')
 const { PORT } = require('./src/config/envs')
 
 server.listen(PORT, () => {
-    console.log(`listen on port ${PORT}`)
+    console.log(`listen on port ${PORT} TODO: dockerizar de nuevo`)
 })
