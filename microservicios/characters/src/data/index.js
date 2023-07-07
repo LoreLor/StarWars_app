bba9db73ab3209d67c4a2cd83c8de2aae38ac6b5
Lorena De Armas <lorenadearmas01@gmail.com>
2023-07-06 21:07:49 -0300

    change_name_localhost_planet_character

--- microservicios/characters/src/data/index.js
+++ microservicios/characters/src/data/index.js
@@ -2,15 +2,15 @@ const axios = require('axios')
 
 module.exports = {
     list: async () => {
-        return await axios.get('http://localhost:7004/Character')
+        return await axios.get('http://db:7004/Character')
     },
 
     getById: async (_id) => {
-        return await axios.get(`http://localhost:7004/Character/${_id}`)
+        return await axios.get(`http://db:7004/Character/${_id}`)
     },
 
     create: async (body) => {
-        return await axios.post('http://localhost:7004/Character', body)
+        return await axios.post('http://db:7004/Character', body)
         // throw new Error('Character not found')
     }
 }
--- microservicios/planets/src/data/index.js
+++ microservicios/planets/src/data/index.js
@@ -2,13 +2,13 @@ const axios = require('axios')
 
 module.exports = {
     list: async () => {
-        return await axios.get('http://localhost:7004/Planet')
+        return await axios.get('http://db:7004/Planet')
     },
     getById: async (_id) => {
-        return await axios.get(`http://localhost:7004/Planet/${_id}`)
+        return await axios.get(`http://db:7004/Planet/${_id}`)
     },
     create: async (body) => {
-        return await axios.post('http://localhost:7004/Planet', body)
+        return await axios.post('http://db:7004/Planet', body)
         // throw new Error('Ocurred some error with your post')
     }
 }
