7a9cb3b3ac071ea5b39be650364f73dffe4ef5a5
Lorena De Armas <lorenadearmas01@gmail.com>
2023-07-06 21:05:43 -0300

    change_name_localhost

--- microservicios/films/src/data/index.js
+++ microservicios/films/src/data/index.js
@@ -2,15 +2,15 @@ const axios = require('axios')
 
 module.exports = {
     list: async () => {
-        return await axios.get('http://localhost:7004/Film')
+        return await axios.get('http://db:7004/Film')
     },
 
     getById: async (_id) => {
-        return await axios.get(`http://localhost:7004/Film/${_id}`)
+        return await axios.get(`http://db:7004/Film/${_id}`)
     },
 
     create: async (body) => {
-        return await axios.post('http://localhost:7004/Film', body)
+        return await axios.post('http://db:7004/Film', body)
         // throw new Error('Film not found')
     }
 }
