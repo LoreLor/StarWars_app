32edd3ed7363ea6eb58799859612fcd8f1721c3b
Lorena De Armas <lorenadearmas01@gmail.com>
2023-07-08 11:52:06 -0300

    cors

--- microservicios/characters/src/server.js
+++ microservicios/characters/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const server = express()
 
 server.use(express.json())
 server.use(morgan('dev'))
+server.use(cors())
 
 server.use('/characters', require('./routes'))
 
