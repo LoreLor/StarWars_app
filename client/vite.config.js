ede4257a004ccbde1517ca9ee7024d1f0c80a8f6
Lorena De Armas <lorenadearmas01@gmail.com>
2023-07-14 15:51:23 -0300

    vite.config2

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,
+    strictPort: true,
     port: 5173,
     watch: {
       usePolling: true
