25cebe49f67f57b60f0158ab0d6caa4e58bf08e9
Lorena De Armas <lorenadearmas01@gmail.com>
2023-07-14 18:03:29 -0300

    port_out_env

--- microservicios/db/index.js
+++ microservicios/db/index.js
@@ -1,6 +1,6 @@
 const server = require('./src/server');
-const { PORT } = require('./src/config/envs')
 
-server.listen(PORT, () => {
-    console.log(`DATABASE run on port: ${PORT}`);
+
+server.listen(7004, () => {
+    console.log('DATABASE run on port: 7004');
 })
